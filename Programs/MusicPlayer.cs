14e87853492c791150889fdb0a8dc7bf7afaa74b
nathanyost15 <nathanyost15@gmail.com>
2017-06-05 16:57:42 -0400

    Readded MusicPlayer.cs

---
+++ Programs/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+public class MusicPlayer
+{
+	private ArrayList sources;
+	public MusicPlayer(params string[] s)
+	{
+		sources = new ArrayList ();
+		foreach (string source in s) 
+			add (source);
+	}
+
+	public void add(string source)
+	{
+		sources.Add (source);
+	}
+}
